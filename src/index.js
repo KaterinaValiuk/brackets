65b1035d24ae75f3535f9a7c5506dbcca8c5a6de
KaterinaValiuk <e5054404@gmail.com>
2021-08-30 15:43:49 +0300

    docs: 13/7

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 /*jshint esversion: 8 */
 
-const openBrs = ['(', '{', '|'];
+
+//let str = '|()|';
+
+const openBrs = ['(', '{', '|', '['];
   const brsPair = {
     [')']: '(',
     ['}']: '{',
     ['|']: '|',
+    [']']: '[',
   };
 
 module.exports = function check(str, bracketsConfig) {
+
+//function check() {
   let stack = [];
 
   for (let i = 0; i < str.length; i++) {
     let currentSymbol = str[i];
     if (openBrs.includes(currentSymbol)) {
       stack.push(currentSymbol);
-    } else {
+      } else {
       if (stack.length === 0) {
-        return false;
+        return(false);
       }
       let topElement = stack[stack.length - 1];
 
       if (brsPair[currentSymbol] === topElement) {
         stack.pop();
       } else {
-        return false;
+        return(false);
       }
-       
+      
     }
   }
-
+//console.log(stack);
   return stack.length === 0;
 };
+
+//check (str);
