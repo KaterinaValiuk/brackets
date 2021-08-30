d110260008c39ba1f7b56e76002a8521ca2d4945
KaterinaValiuk <e5054404@gmail.com>
2021-08-30 15:13:25 +0300

    docs: task was not finished. 11/9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+/*jshint esversion: 8 */
+
+const openBrs = ['(', '{', '|'];
+  const brsPair = {
+    [')']: '(',
+    ['}']: '{',
+    ['|']: '|',
+  };
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    if (openBrs.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topElement = stack[stack.length - 1];
+
+      if (brsPair[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+       
+    }
+  }
+
+  return stack.length === 0;
+};
